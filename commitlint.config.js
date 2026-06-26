777b0542dd1981c1f05cd566c388841f4224e528
Xiou.Wang <xiou.wang@feisu.com>
2026-06-26 16:07:13 +0800

    feat: 补充

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,24 +6,24 @@ export default {
       2,
       'always',
       [
-        'feat',     // 新功能
-        'fix',      // 修复 Bug
-        'docs',     // 文档更新
-        'style',    // 代码格式（不影响代码运行）
+        'feat', // 新功能
+        'fix', // 修复 Bug
+        'docs', // 文档更新
+        'style', // 代码格式（不影响代码运行）
         'refactor', // 重构（既不是新增功能，也不是修复 Bug）
-        'perf',     // 性能优化
-        'test',     // 测试相关
-        'build',    // 构建工具或外部依赖变更（如 webpack、vite、npm）
-        'ci',       // CI 配置或脚本变更
-        'chore',    // 杂项（不修改 src 或 test）
-        'revert',   // 回滚提交
-      ],
+        'perf', // 性能优化
+        'test', // 测试相关
+        'build', // 构建工具或外部依赖变更（如 webpack、vite、npm）
+        'ci', // CI 配置或脚本变更
+        'chore', // 杂项（不修改 src 或 test）
+        'revert' // 回滚提交
+      ]
     ],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72],
-  },
-};
+    'header-max-length': [2, 'always', 72]
+  }
+}
